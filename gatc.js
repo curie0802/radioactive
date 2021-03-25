f0fbd2bd4d001279b72d655139995670c1bd9171
curie0802 <81336453+curie0802@users.noreply.github.com>
2021-03-25 10:17:49 +0800

    Update gatc.js

--- gatc.js
+++ gatc.js
@@ -1,31 +1,34 @@
-function analytics() {
-  let analytics_configUrl = document.getElementById("config").value;
-  let analytics_originalTag = document.getElementById("originaltag").value;
+function gatc() {
+  let gatc_configUrl = document.getElementById("config").value;
+  let gatc_originalTag = document.getElementById("originaltag").value;
 
-  let analytics_UAIDRegex = /UA-\d/g;
+  let gatc_UAIDRegex = /UA-\d/g;
 
-  let analytics_arrUAID = analytics_originalTag.match(analytics_UAIDRegex) || 0;
+  let gatc_arrUAID = gatc_originalTag.match(analytics_UAIDRegex) || 0;
   console.log(
-    analytics_arrUAID
+gatc_arrUAID
   );
 
-  let analytics_strSlot = analytics_arrAdSlot.toString().replaceAll('slot="', "");
-  console.log(analytics_strWidth, analytics_strHeight, analytics_strSlot);
+let analytics_strSlot = analytics_arrAdSlot.toString().replaceAll('slot="', "");
+  console.log(gatc_strWidth, gatc_strHeight, gatc_strSlot);
 
-  let analytics_resultingTag = `
-<script>
-window.pg=window.pg||[];pg.acq=pg.acq||[]; // build consent queue
-pg.acq.push(function() {
-// Global site tag (gtag.js)
-pg.loadJS("https://www.googletagmanager.com/gtag/js?id='${analytics_arrUAID}'");
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '${analytics_arrUAID}');
-});
-</script>
-    `;
+  let gatc_resultingTag = `
+  <script type="text/javascript">
+  window.pg=window.pg||[];pg.acq=pg.acq||[];
+  pg.acq.push(function() {
+  //Google analytics code
+  pg.loadJS("https://www.googletagmanager.com/gtag/js?id='${gatc_arrUAID}'1");
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${gatc_arrUAID}');
+  });
+  </script>
+
+  <script src="${gatc_configUrl}" async></script>
+  <script type="text/javascript" src="//cdn.pubguru.com/fb.js"></script>
+  `;
 
   // set the tag
-  document.getElementById("resulttag").value = analytics_resultingTag.trim();
+  document.getElementById("resulttag").value = gatc_resultingTag.trim();
 }
